082b4b4a9a3af43026827592e17e434a2db3ccdd
pertneer <pertneer@pertneer.net>
2024-11-27 09:26:26 -0600

    Create some sample docs and initial includes

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <secrets.h>
+#include <Arduino.h>
+#include <WiFiManager.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
+
---
+++ src/sample_secrets.h
@@ -0,0 +1,8 @@
+// Initialize Telegram BOT
+#define BOTtoken "xxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxx"  // your Bot Token (Get from Botfather)
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "xxxxxxxxxx"
+
